524524978d9d03bcfbda5a2aaca3b70a275c01ea
Sam Tran <samvtran@gmail.com>
2013-02-02 13:20:48 -0600

    Heroku settings

---
+++ scripts/controllers/writing.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular.module('personalApp').controller('WritingCtrl', [
+    '$scope', function($scope) {
+      $scope.articles = [
+        {
+          title: 'Torchlight: Diablo-Inspired Action RPG',
+          href: 'http://www.omgubuntu.co.uk/2012/12/torchlight-linux-review',
+          date: '27 December 2012'
+        }, {
+          title: 'Sidescroller Run Off: Bit.Trip Runner Vs Canabalt',
+          link: 'http://www.omgubuntu.co.uk/2012/12/sidescroller-run-off-bit-trip-runner-vs-canabalt-review',
+          date: '9 December 2012'
+        }, {
+          title: 'Hands on With \'RC Mini Racers \' for Ubuntu',
+          link: 'http://www.omgubuntu.co.uk/2012/12/rc-mini-racers-ubuntu-review',
+          date: '3 December 2012'
+        }
+      ];
+      return $scope.writings = [
+        {
+          title: 'Reservoir',
+          date: '10 May 2012',
+          link: 'reservoir'
+        }
+      ];
+    }
+  ]);
+
+}).call(this);
