bad9eeba3b386cc4117ba0c2742555adc8a6dac5
Sam Tran <samvtran@gmail.com>
2013-04-12 18:47:52 -0500

    Fixed path issue on CS output

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -2,34 +2,23 @@
 (function() {
   'use strict';
 
-  angular.module('personalApp').controller('AppCtrl', [
-    '$scope', '$location', function($scope, $location) {
-      $scope.navigation = [
-        {
-          title: 'Home',
-          link: '/'
-        }, {
-          title: 'About',
-          link: '/about'
-        }, {
-          title: 'Recent Projects',
-          link: '/projects'
-        }, {
-          title: 'Writing',
-          link: '/writing'
-        }, {
-          title: 'Social',
-          link: '/social'
-        }
-      ];
-      return $scope.currentUrl = function() {
-        return $location.path();
-      };
-    }
-  ]);
-
-}).call(this);
-'SocialCtrl'
+  angular.module('personalApp', []).config([
+    '$routeProvider', function($routeProvider) {
+      return $routeProvider.when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      }).when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      }).when('/projects', {
+        templateUrl: 'views/projects.html',
+        controller: 'ProjectsCtrl'
+      }).when('/writing', {
+        templateUrl: 'views/writing.html',
+        controller: 'WritingCtrl'
+      }).when('/social', {
+        templateUrl: 'views/social.html',
+        controller: 'SocialCtrl'
       }).when('/writing/:work', {
         templateUrl: 'views/work.html',
         controller: 'WorkCtrl'
